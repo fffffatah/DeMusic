0155a32362acedf4fec0dee5e3216a417ae7fcff
Muhammad Fatah <ab.fatahmn@hotmail.com>
2020-03-19 21:41:49 +0600

    minor changes

---
+++ DeMusic/CheckAppDataLogic.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Windows;
+
+namespace DeMusic
+{
+    public partial class ControlAllPageComponent
+    {
+        //THIS METHOD CHECKS FOR THE DB FILE IN APPDATA
+        public bool CheckData()
+        {
+            if (File.Exists(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + @"\DeMusic\songdatabase.db"))
+            {
+                mainPageObj.lonelyPageFrame.Visibility = Visibility.Hidden;
+                ChangeButtonState(false);
+                return true;
+            }
+            else
+            {
+                mainPageObj.lonelyPageFrame.Visibility = Visibility.Visible;
+                ChangeButtonState(true);
+                return false;
+            }
+        }
+    }
+}
